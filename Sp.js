5d4fb3834b5a06686d38624ada742662bcb61b0c
pawitsahare0 <119015232+pawitsahare0@users.noreply.github.com>
2023-01-21 17:19:08 +0530

    Update Sp.js

--- Sp.js
+++ Sp.js
@@ -6,12 +6,23 @@ const fs = require('fs')
 
 const port = 3000
 
-app.get('/', (req, res) => {
-    const url
+app.get('/', async (req, res) => {
+
+    const {url} = req.query
+
+    
+
+    if (!url) {
+
+         return res.send("url miss")
+    } else {
+
+        // generate puppeteer screenshot 
+    
     const readable = fs.createReadStream(url)
+}
 
-})
-
+}
 app.listen(port, () => {
 
     console.log('listening on port ' + port)
