418e567dccfce96f19f91abfdf51cbb35e0ab96d
pawitsahare0 <119015232+pawitsahare0@users.noreply.github.com>
2023-01-21 17:12:39 +0530

    Create Sp.js

---
+++ Sp.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+const fs = require('fs')
+
+
+
+const port = 3000
+
+app.get('/', (req, res) => {
+    const url
+    const readable = fs.createReadStream(url)
+
+})
+
+app.listen(port, () => {
+
+    console.log('listening on port ' + port)
+
+})
